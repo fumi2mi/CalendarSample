3851ed4a2d732c06c903d4e3bd142e0f1e843cb3
fumi2mi <imafumi.murataka@gmail.com>
2021-11-23 23:05:51 +0900

    TableViewで31行を表示する

--- CalendarSample/ViewController.swift
+++ CalendarSample/ViewController.swift
@@ -17,3 +17,26 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        31
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = "\(indexPath.row + 1)"
+            cell.contentConfiguration = content
+        } else {
+            cell.textLabel?.text = "\(indexPath.row + 1)"
+        }
+        return cell
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate {
+
+}
